959e7d07c565b76e6afcabbd7e24b7768f2db194
Louis Andris <louis.andris@dhanapala.id>
2021-08-23 12:02:30 +0700

    add example

---
+++ examples/http-example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	im "github.com/dhanapala-id/go-idempotency"
+	rs "github.com/dhanapala-id/go-idempotency/store/redis"
+
+	"github.com/go-zoo/bone"
+)
+
+func init() {
+	im.UseStore(rs.New("127.0.0.1:6379", "", 0))
+}
+
+func main() {
+	mux := bone.New()
+
+	mux.Post("/check-idempotency", im.Check(http.HandlerFunc(simulateIdempotency)))
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func simulateIdempotency(w http.ResponseWriter, r *http.Request) {
+	// simulate
+	sleep, _ := strconv.Atoi(r.FormValue("sleep"))
+	if sleep > 0 {
+		time.Sleep(time.Duration(sleep) * time.Second)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("hello world!"))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/dhanapala-id/go-idempotency
 
 go 1.16
+
+replace github.com/dhanapala-id/go-idempotency/store => ./store
+
+require github.com/dhanapala-id/go-idempotency/store v0.0.0-00010101000000-000000000000
